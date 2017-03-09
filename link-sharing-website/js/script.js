ac7c5e0bb6feb6f4beae5b9229048f9c3d71cb28
A. L. H <audelise.h@gmail.com>
2017-03-09 15:41:25 +0100

    it's now possible for users to dynamically add a (temporary) link

--- link-sharing-website/js/script.js
+++ link-sharing-website/js/script.js
@@ -56,7 +56,7 @@ var skip = document.createElement("br");
 liensElt.appendChild(skip);
 
 // Ajout du commentaire  
-var comment = document.createElement("span");
+let comment = document.createElement("span");
 comment.textContent = liens.comment;
 liensElt.appendChild(comment);
 document.getElementById("contenu").appendChild(liensElt); 
@@ -66,7 +66,7 @@ var skip = document.createElement("br");
 liensElt.appendChild(skip);
   
 // Creation du tag span et ajout d'un texte précédant l'auteur
-var ajout = document.createElement("span");
+const ajout = document.createElement("span");
 ajout.textContent = "added by " + liens.auteur;
 ajout.style.textTransform = "lowercase";
 ajout.style.fontSize = "xx-small";
@@ -79,6 +79,87 @@ return liensElt;
 
 //Application de la fonction à chaque lien de l'objet
 listeLiens.forEach(function(lien){
-  var elementLien = creationLien(lien);
+  const elementLien = creationLien(lien);
   document.getElementById("contenu").appendChild(elementLien);
+});
+
+//ajout de l'input
+
+function newInput (placeholder, size) {
+const input = document.createElement("input");
+input.type = "text";
+input.setAttribute("placeholder", placeholder);
+input.setAttribute("size", size);
+input.setAttribute("required", "true");
+return input;
+}
+
+//création d'un évènement en cliquant sur le bouton
+
+const ajoutBtn = document.getElementById("ajoutBtn");
+
+ajoutBtn.addEventListener("click", function() {
+    let auteurElt = newInput("Your name", 6);
+    let titreElt = newInput("Title", 8);
+    let lienElt = newInput("URL", 14);
+    let comElt = newInput("Add a comment", 20);
+
+    let ajout = document.createElement("input");
+    ajout.type = "submit";
+    ajout.value = "Ajouter le lien";
+    ajout.classList.add("addLink");
+
+    let form = document.createElement("form");
+    form.appendChild(auteurElt);
+    form.appendChild(titreElt);
+    form.appendChild(lienElt);
+    form.appendChild(comElt);
+    form.appendChild(ajout);
+
+let p = document.querySelector("p");
+p.replaceChild(form, ajoutBtn);
+
+//aucune action sur les champs ne sont pas complétés
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+//ajout de l'entête "http" s'il est manquant, ou si "https" est manquant
+
+let url = lienElt.value;
+if((url.indexOf("http://") !== false) && (url.indexOf("https://") !== false)) {
+    url = "http://" + url;
+}
+
+//creation d'un nouvel objet
+
+let newLien = {
+    titre: titreElt.value,
+    url: url,
+    auteur: auteurElt.value,
+    comment: comElt.value
+};
+
+let lienAjout = creationLien(newLien);
+let contenu = document.getElementById("contenu");
+contenu.insertBefore(lienAjout, contenu.firstChild);
+
+//supression du bouton
+
+p.replaceChild(ajoutBtn, form);
+
+// ajout d'un message temporaire indiquant que le lien a bien été ajouté
+
+let success = document.createElement("div");
+success.classList.add("info");
+success.textContent = "Congrats! " + newLien.titre + " has been added!";
+p.insertBefore(success, ajoutBtn);
+
+setTimeout(function () {
+    p.removeChild(success);
+}, 4000);
+
+});
+
+
 });
